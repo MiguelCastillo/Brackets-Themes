5147ce76bbd550f60b6f8d69796ec2f5c5f4a635
Miguel Castillo <manchagnu@gmail.com>
2014-01-31 11:05:16 -0500

    Checking in changes to support custom directory for themes

--- main.js
+++ main.js
@@ -1,25 +1,7 @@
 /*
- * Copyright (c) 2014 Miguel Castillo.
+ * Brackets Themes Copyright (c) 2014 Miguel Castillo.
  *
  * Licensed under MIT
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
  */
 
 
@@ -53,10 +35,13 @@ define(function (require, exports, module) {
         $.getScript(FileUtils.getNativeBracketsDirectoryPath() + "/thirdparty/CodeMirror2/addon/search/match-highlighter.js").promise(),
 
         // Load up all the theme files from custom themes directory
-        themeManager.loadFiles(require.toUrl('./theme/')),
+        themeManager.loadFiles(require.toUrl("./") + "../../themes"),
+
+        // Load up all the theme files from custom themes directory
+        themeManager.loadFiles(require.toUrl("./theme/")),
 
         // Load up all the theme files from codemirror themes directory
-        themeManager.loadFiles(cm_path + '/theme')
+        themeManager.loadFiles(cm_path + "/theme")
     ];
 
 
